32f4889bd4d2ed74fee099a4d74c4bc4b8be5af6
Habibur Rahman <habibur88454@gmail.com>
2024-09-06 00:08:29 +0600

    js return apply

---
+++ return.js
@@ -0,0 +1,16 @@
+function tenTimes(num) {
+    const tenTimes = num * 10
+    return tenTimes;
+}
+
+
+const result= tenTimes(10);
+console.log(result)
+
+function half(num){
+const result = num / 2;
+return result;
+}
+
+const output = half(40);
+console.log(output)
