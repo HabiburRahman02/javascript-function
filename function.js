94ea9c49f9acda65e29ece3b86407c49fa423979
Habibur Rahman <habibur88454@gmail.com>
2024-09-05 22:32:38 +0600

    try to how declare a func

---
+++ function.js
@@ -0,0 +1,17 @@
+
+function myFunction(){
+    console.log('how are func.');
+    console.log('function is very important thinks in js')
+}
+
+myFunction();
+
+console.log('i am a innocent boy no doubt');
+console.log('i will be a good developer');
+
+
+function myAgency(){
+    console.log('my dream one day i have i big web company')
+}
+
+myAgency();
