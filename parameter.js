841895080dc869b7a2e69009cc18f2758e34357a
Habibur Rahman <habibur88454@gmail.com>
2024-09-05 23:25:10 +0600

    apply js parameter

---
+++ parameter.js
@@ -0,0 +1,25 @@
+function square(num) {
+    const square = num * num
+    console.log(`Squre of ${num}:`, square);
+}
+
+square(2);
+square(7);
+square(12);
+
+
+function add(num1, num2) {
+    const total = num1 + num2;
+    console.log(total)
+}
+
+add(2, 34)
+add(2, 4)
+
+
+
+function sumAll(a, b, c, d, e) {
+    const totalNum = a + b + c + d + e;
+    console.log('total num: ', totalNum)
+}
+sumAll(1, 3, 5, 78, 6)
