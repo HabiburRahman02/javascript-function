d54c91ca64f7579ee11e360a8cb78067eb264012
Habibur Rahman <habibur88454@gmail.com>
2024-09-05 23:47:54 +0600

    recap func

---
+++ recap.js
@@ -0,0 +1,19 @@
+function diff(num1, num2) {
+    const difference = num1 - num2;
+    console.log(`Difference:`, difference)
+}
+
+diff(33,12)
+const friendAge = 23;
+const myAge = 20;
+diff(friendAge,myAge)
+
+
+function doubleIt(num){
+const double = num * 2
+console.log(double);
+}
+
+doubleIt(6)
+console.log('--------------')
+doubleIt(8);
